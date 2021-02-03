982f64379869427dcd5e476e6df04860ffd13db8
Mani Kumar <purammanikumar@gmail>
2021-02-04 00:29:21 +0530

    modified

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-class NavBar extends Component {
-    // state = {  }
-    render() { 
-        return ( <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1"><strong>Navigation Bar <span className="badge badge-secondary sm m-2">{this.props.totalCounters}</span></strong></span>
-      </nav>
-         );
-    }
+
+// const NavBar = () =>{
+
+// };
+// class NavBar extends Component {
+//     // state = {  }
+//     render() {
+//         return (<nav class="navbar navbar-light bg-light">
+//             <span class="navbar-brand mb-0 h1"><strong>Navigation Bar <span className="badge badge-secondary sm m-1">{this.props.totalCounters}</span></strong></span>
+//         </nav>
+//         );
+//     }
+// }
+
+// export default NavBar;
+const NavBar = ({ totalCounters }) => {
+    return (<nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1"><strong>Navigation Bar <span className="badge badge-secondary sm m-1">{totalCounters}</span></strong></span>
+    </nav>
+
+    );
 }
- 
+
 export default NavBar;
